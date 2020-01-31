fd668a5011c84dc8a91a5d93f72a3e8c4fbf4de0
kan <erkan.dem@pm.me>
2020-01-31 21:18:55 +0100

    - added type hint

--- swagccg/src/make_client.py
+++ swagccg/src/make_client.py
@@ -22,7 +22,7 @@ except ImportError:
 PARSED_HTTP_METHODS = ['GET', 'POST', 'DELETE', 'PATCH', 'PUT']
 
 
-def convert_to_snake_case(name):
+def convert_to_snake_case(name: str) -> str:
     """via https://stackoverflow.com/a/1176023 """
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
